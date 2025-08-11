495013bbf08e8aca51b8b2e0c620c66b1e88c2bb
Gilles Radrizzi <Gilles.Radrizzi@weiswampach.lu>
2025-08-11 12:53:52 +0200

    Initial commit - Chapter 6

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+  return <>
+    <div className="container mx-auto px-4">
+      <h1 className="text-2xl font-bold">Welcome to Tailwind CSS</h1>
+      <p className="mt-4">This is a simple example demonstrating the use of the container class in Tailwind CSS.</p>
+    </div>
+  </>
+}
